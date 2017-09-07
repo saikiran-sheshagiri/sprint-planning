186f217fda776710aad4d06213f1ae2413480da1
Saikiran Sheshagiri <saikiran.sheshagiri@gmail.com>
2017-09-07 13:45:15 -0500

    updated port

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var _ = require('lodash');
+var process = require('process');
+
+var port = process.env.PORT || 3000;
+
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
@@ -91,6 +95,6 @@ io.on('connection', function (socket) {
 
 });
 
-http.listen(3000, function () {
-	console.log('listening on *:3000');
+http.listen(port, function () {
+	console.log('listening on *:port');
 });
